3c70bab6b5e609d2d33ed241981b3dfbb51f6958
cxnub <2302241A@student.tp.edu.sg>
2025-08-05 13:37:12 +0800

    Added missing Flask import

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, current_app
+from flask import Blueprint, request, jsonify, current_app, Flask
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from flask_cors import CORS
 
